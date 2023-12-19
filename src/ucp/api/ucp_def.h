4313cac4357dd79dad1063d2af18294c7598a573
Roie Danino <rdanino@nvidia.com>
2023-12-19 20:52:58 +0200

    UCP/API: Max priority is queried in ucp_context_query and being set at ucp_worker_create

--- src/ucp/api/ucp_def.h
+++ src/ucp/api/ucp_def.h
@@ -760,7 +760,13 @@ typedef struct ucp_ep_params {
      * UCP_EP_PARAM_FIELD_LOCAL_SOCK_ADDR bit in the field mask must be set.
      */
     ucs_sock_addr_t         local_sockaddr;
-
+    
+    /**
+     * Number of message priority levels, limited by 
+     * This setting is optional. To enable it, the corresponding - @ref 
+     * UCP_EP_PARAM_FIELD_NUM_PRIORITIES bit in the field mask must be set.
+     */
+    uint8_t                 num_priorities;
 } ucp_ep_params_t;
 
 
